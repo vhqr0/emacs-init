043e9cbfc9b2798fafbfc085bee562054949835f
vhqr0 <zq_cmd@163.com>
2026-01-16 19:27:14 +0800

    w32 dark mode

--- misc/env-windows.el
+++ misc/env-windows.el
@@ -1,5 +1,7 @@
 ;;; -*- lexical-binding: t; no-native-compile: t -*-
 
+(setq w32-follow-system-dark-mode t)
+
 (setenv "GIT_ASKPASS" "git-gui--askpass")
 (setenv "SSH_ASKPASS" "git-gui--askpass")
 
